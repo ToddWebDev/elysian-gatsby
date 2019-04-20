552ca3cee412ed620a59c6d65e60722094ffdc26
ToddWebDev <webdevtodd@gmail.com>
2019-04-20 17:43:45 -0400

    Add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+const NotFound = () => {
+  return (
+    <Layout>
+      <h1>Page not found</h1>
+      <p>This is not the page you're looking for.</p>
+    </Layout>
+  )
+}
+
+export default NotFound
